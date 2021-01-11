192d8e837feec38bbb5c203994d61eccc338a22c
Dian CHEN <chendian2017@gmail.com>
2021-01-11 09:36:10 +0100

    Tree update

--- Graph/Tree/Traversal_2.py
+++ Graph/Tree/Traversal_2.py
@@ -1,7 +1,94 @@
 ## Leetcode 654. Maximum Binary Tree
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def constructMaximumBinaryTree(self, nums: List[int]) -> TreeNode:
+        return self.build(nums, 0, len(nums)-1)
+
+
+    def build(self, nums, l, h):
+        # base case:
+        if l > h:
+            return None
+
+        # pre-order, build the root
+        index = 0
+        maxVal = -float("inf")
+        for i in range(l,h+1):
+            if maxVal < nums[i]:
+                maxVal = nums[i]
+                index = i
+                break
+
+        root = TreeNode(maxVal)
+        root.left = self.build(nums, l, index-1)
+        root.right = self.build(nums, index+1, h)
+
+        return root
 
 
 ## Leetcode 105. Construct Binary Tree from Preorder and Inorder Traversal
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+        return self.build(preorder, 0, len(preorder)-1,
+                            inorder, 0, len(inorder)-1)
+
+    def build(self, preorder, preStart, preEnd,
+                    inorder, inStart, inEnd):
+        # basic case:
+        if preStart > preEnd:
+            return None
+        # build root
+        rootVal = preorder[preStart]
+        index = 0
+        for i in range(inStart, inEnd):
+            if inorder[i] == rootVal:
+                index = i
+                break
+
+        leftSize = index - inStart
+        root = TreeNode(rootVal)
+        root.left = self.build(preorder, preStart+1, preStart+leftSize,
+                                inorder, inStart, index-1)
+        root.right = self.build(preorder, preStart+leftSize+1, preEnd,
+                                inorder, index+1, inEnd)
+        return root
+
 
 
 ## Leetcode 106. Construct Binary Tree from Inorder and Postorder Traversal
+class Solution:
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> TreeNode:
+        return self.build(inorder, 0, len(inorder)-1,
+                        postorder, 0, len(postorder)-1)
+        
+        
+    def build(self, inorder, inStart, inEnd,
+                    postorder, postStart, postEnd):
+        # basic case:
+        if postStart > postEnd:
+            return None
+        # build the root
+        rootVal = postorder[postEnd]
+        root = TreeNode(rootVal)
+        
+        # Find the index of root in inorder
+        index = 0
+        for i in range(inStart, inEnd+1):
+            if inorder[i] == rootVal:
+                index = i
+                break
+                
+        # Find the size of left tree
+        sizeLeft = index - inStart
+            
+        root.left = self.build(inorder, inStart, index-1,
+                            postorder, postStart, postStart+sizeLeft-1)
+        root.right = self.build(inorder, index+1, inEnd,
+                            postorder, postStart+sizeLeft, postEnd-1)
+        return root
+        
--- Graph/Tree/Traversal_3.py
+++ Graph/Tree/Traversal_3.py
@@ -1,1 +1,32 @@
 ## Leetcode 652. Find Duplicate Subtrees
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+from collections import defaultdict
+class Solution:
+    def findDuplicateSubtrees(self, root: TreeNode) -> List[TreeNode]:
+        self.dict = defaultdict(int)
+        self.res = []
+        self.traverse(root)
+        return self.res
+
+    def traverse(self, root):
+        # basic case
+        if root is None:
+            return "#"
+
+        # Build the subTree and its identifier
+        left = self.traverse(root.left)
+        right = self.traverse(root.right)
+        subTree = left + "," + right + "," + str(root.val)
+
+        # Compare with other subTree in the dict
+        if self.dict[subTree] == 1:
+            self.res.append(root)
+        self.dict[subTree] += 1
+
+        return subTree
+    
