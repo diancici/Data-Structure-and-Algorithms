cd19ca445b4ac077b16e8d7b181cc1df6539ce8e
Dian CHEN <chendian2017@gmail.com>
2021-01-14 18:52:22 +0100

    Update Nnodes Tree

--- Graph/Tree/Nnodes_Tree.py
+++ Graph/Tree/Nnodes_Tree.py
@@ -10,4 +10,19 @@ def traverse(root: TreeNode):
         traverse(child)
 
 ## Leetcode 341. Flatten Nested List Iterator
+class NestedIterator:
+    def __init__(self, nestedList: List):
+        self.stack = nestedList[::-1]
+
+    def next(self)->int:
+        return self.stack.pop().getInteger()
+
+    def hasNext(self) -> bool:
+        while(self.stack):
+            top = self.stack[-1]
+            if top.isInteger():
+                return True
+            self.stack = self.stack[:-1] + top.getList()[::-1]
+        return False
+
 
