5ddbf29a53637f8048dffa1aba631c85303d10b3
Dian CHEN <chendian2017@gmail.com>
2021-01-25 20:14:27 +0100

    DFS

--- Algo/Search/DFS/Backtracking_1.py
+++ Algo/Search/DFS/Backtracking_1.py
@@ -39,8 +39,51 @@ class Solution:
                 visited.pop()
 
 
-s = Solution()
-nums = [1,2,3]
-print(s.permute(nums))
+# s = Solution()
+# nums = [1,2,3]
+# print(s.permute(nums))
 
 ## Leetcode 51. N-Queens
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        board = [[0] * n] * n # inialize the board
+        self.res = []
+        self.backtrack(board, 0)
+        return self.res
+    
+    # path/board: rows less than row have queens successfully
+    # choices: All coloumns of this row can be put a queen.
+    # end condition: row is over the len pg board
+    def backtrack(self, board, row):
+        # trigger end condition
+        if row == len(board):
+            self.res.append(board[:])
+            return
+        
+        n = len(board[row])
+        for col in range(n):
+            # Rule out unreasonable choices
+            if (self.isValid(board,row, col) == False):
+                continue
+            # make a choice
+            board[row]= '.'*col+'Q'+'.'*(n-col-1)
+            # move on to the next step
+            self.backtrack(board, row+1)
+            # Cancel selection
+            board[row]= '.'*n
+            
+    def isValid(self, board, row, col):
+        n = len(board)
+        # check conflit in a column
+        for i in range(n):
+            if board[i][col] == 'Q':
+                return False
+        # check conflit in upper right
+        for i, j in zip(range(row-1, -1, -1), range(col+1,n)):        
+            if board[i][j] == 'Q':
+                return False
+        # check conflit in upper left:
+        for i, j in zip(range(row-1, -1, -1), range(col-1,-1,-1)):  
+            if board[i][j] == 'Q':
+                return False
+        return True
